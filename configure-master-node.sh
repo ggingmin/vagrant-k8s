ea0f3ccb726c24cdaf21d5a96b44efb360616820
ggingmin <ggingmin@gmail.com>
2021-12-20 19:18:36 +0900

    script edited

--- configure-master-node.sh
+++ configure-master-node.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -e
 
-master_node=172.16.8.10
-pod_network_cidr=192.168.0.0/16
+MASTER_NODE=172.16.8.10
+POD_NETWORK_CIDR=192.168.0.0/16
 
 initialize_master_node ()
 {
 sudo systemctl enable kubelet
 sudo kubeadm config images pull
-sudo kubeadm init --apiserver-advertise-address=$master_node --pod-network-cidr=$pod_network_cidr --ignore-preflight-errors=NumCPU
+sudo kubeadm init --apiserver-advertise-address=$MASTER_NODE --pod-network-cidr=$POD_NETWORK_CIDR --ignore-preflight-errors=NumCPU
 }
 
 create_join_command ()
@@ -19,13 +19,8 @@ chmod +x /vagrant/join_command.sh
 configure_kubectl () 
 {
 mkdir -p $HOME/.kube
-sudo cp -f /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-##For vagrant user
-mkdir -p /home/vagrant/.kube
-sudo cp -f /etc/kubernetes/admin.conf /home/vagrant/.kube/config
-sudo chown 900:900 /home/vagrant/.kube/config
 }
 
 install_network_cni ()
@@ -33,7 +28,54 @@ install_network_cni ()
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 }
 
+install_metrics_server ()
+{
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+}
+
+install_kubernetes_dashboard ()
+{
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
+}
+
+# create_dashboard_user ()
+# {
+# cat <<EOF | kubectl apply -f -
+# apiVersion: v1
+# kind: ServiceAccount
+# metadata:
+#   name: admin-user
+#   namespace: kubernetes-dashboard
+# EOF
+
+# cat <<EOF | kubectl apply -f -
+# apiVersion: rbac.authorization.k8s.io/v1
+# kind: ClusterRoleBinding
+# metadata:
+#   name: admin-user
+# roleRef:
+#   apiGroup: rbac.authorization.k8s.io
+#   kind: ClusterRole
+#   name: cluster-admin
+# subjects:
+# - kind: ServiceAccount
+#   name: admin-user
+#   namespace: kubernetes-dashboard
+# EOF
+
+# sudo -i -u vagrant bash << EOF
+# mkdir -p /home/vagrant/.kube
+# sudo cp -i /vagrant/configs/config /home/vagrant/.kube/
+# sudo chown 1000:1000 /home/vagrant/.kube/config
+# EOF
+# }
+
+
+
 initialize_master_node
 configure_kubectl
 install_network_cni
+install_metrics_server
+install_kubernetes_dashboard
 create_join_command
+# create_dashboard_user
--- join_command.sh
+++ join_command.sh
@@ -1,1 +1,1 @@
-kubeadm join 172.16.8.10:6443 --token p7fnek.rsl39cwns8haq3t2     --discovery-token-ca-cert-hash sha256:208bdc7ca992faa5902b7b37a38e40fb3f45bb21dbdaaa58baa99fafb4bf482d 
+kubeadm join 172.16.8.10:6443 --token nrsrxr.eyou7xq2hr9hssrz     --discovery-token-ca-cert-hash sha256:cf1dbc65f42e04379b83c1627e3ceb45540c08181f40138efa64bfbbd68cf801 
